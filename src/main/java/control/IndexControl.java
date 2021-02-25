33dc0143ca6457c067304423cf4f196303ae3f36
Nikoches <nikoches@gmail.com>
2021-02-25 22:57:41 +0300

    Initial commit

---
+++ src/main/java/control/IndexControl.java
@@ -0,0 +1,12 @@
+package control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexControl {
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+}
