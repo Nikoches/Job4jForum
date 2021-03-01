0cb23a81f5496c480f5f2bd6e961e575c913c641
Nikoches <nikoches@gmail.com>
2021-03-01 21:15:54 +0300

    editing jsp

--- src/main/java/ru/job4j/forum/control/IndexControl.java
+++ src/main/java/ru/job4j/forum/control/IndexControl.java
@@ -1,12 +1,22 @@
 package ru.job4j.forum.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.job4j.forum.service.PostService;
 
-@Controller
+@RestController
 public class IndexControl {
-    @GetMapping({"/","/index"})
-    public String index() {
+    private final PostService posts;
+
+    public IndexControl(PostService posts) {
+        this.posts = posts;
+    }
+
+    @GetMapping({"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("posts", posts.getAll());
         return "index";
     }
 }
