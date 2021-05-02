6ac0020a78a8b6490df0f1a0ca4244806bfaf849
Nikoches <nikoches@gmail.com>
2021-05-02 13:57:16 +0300

    Test Spring profiles

--- src/test/java/ControllersTest/ConverstionControlTest.java
+++ src/test/java/ControllersTest/ConverstionControlTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 public class ConverstionControlTest {
     @Autowired
     private MockMvc mockMvc;
